57b7662663ab591ad7ff591c592361bf15bc1c69
pranav <125111881+pranav-85@users.noreply.github.com>
2024-07-18 23:09:16 +0530

    Delete NoteIt directory

--- NoteIt/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import notes_list, note_detail
-
-urlpatterns = [
-    path('notes/', notes_list, name='notes_list'),
-    path('notes/<int:pk>/', note_detail, name='note_detail'),
-
-]
